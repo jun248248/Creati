c68a08febed99998b4390af67164982dc340d5bb
jun248248 <20203110@office.deu.ac.kr>
2026-02-12 15:06:42 +0900

    feat:필요 Dto 생성

---
+++ src/main/java/com/creati/dto/LogDto.java
@@ -0,0 +1,123 @@
+package com.creati.dto;
+
+import java.time.LocalDateTime;
+
+/* 필요시 Dto 분리*/
+public class LogDto {
+	
+	private Long id;
+	private String userId;
+	private String title;
+	private String contentTitle;
+	private String contentUrl;
+	private Long platformId;
+	private Long categoryId;
+	private String tryContent;
+	private String resultStatus;
+	private String failResult;
+	private String failReason;
+	private Boolean isPublic;
+	private Boolean isDraft;
+	private Long viewCount;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContentTitle() {
+		return contentTitle;
+	}
+	public void setContentTitle(String contentTitle) {
+		this.contentTitle = contentTitle;
+	}
+	public String getContentUrl() {
+		return contentUrl;
+	}
+	public void setContentUrl(String contentUrl) {
+		this.contentUrl = contentUrl;
+	}
+	public Long getPlatformId() {
+		return platformId;
+	}
+	public void setPlatformId(Long platformId) {
+		this.platformId = platformId;
+	}
+	public Long getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(Long categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getTryContent() {
+		return tryContent;
+	}
+	public void setTryContent(String tryContent) {
+		this.tryContent = tryContent;
+	}
+	public String getResultStatus() {
+		return resultStatus;
+	}
+	public void setResultStatus(String resultStatus) {
+		this.resultStatus = resultStatus;
+	}
+	public String getFailResult() {
+		return failResult;
+	}
+	public void setFailResult(String failResult) {
+		this.failResult = failResult;
+	}
+	public String getFailReason() {
+		return failReason;
+	}
+	public void setFailReason(String failReason) {
+		this.failReason = failReason;
+	}
+	public Boolean getIsPublic() {
+		return isPublic;
+	}
+	public void setIsPublic(Boolean isPublic) {
+		this.isPublic = isPublic;
+	}
+	public Boolean getIsDraft() {
+		return isDraft;
+	}
+	public void setIsDraft(Boolean isDraft) {
+		this.isDraft = isDraft;
+	}
+	public Long getViewCount() {
+		return viewCount;
+	}
+	public void setViewCount(Long viewCount) {
+		this.viewCount = viewCount;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	
+}
---
+++ src/main/java/com/creati/dto/LogKeywordDto.java
@@ -0,0 +1,20 @@
+package com.creati.dto;
+
+public class LogKeywordDto {
+
+	private Long logId;
+    private Long keywordId;
+    
+	public Long getLogId() {
+		return logId;
+	}
+	public void setLogId(Long logId) {
+		this.logId = logId;
+	}
+	public Long getKeywordId() {
+		return keywordId;
+	}
+	public void setKeywordId(Long keywordId) {
+		this.keywordId = keywordId;
+	}
+}
