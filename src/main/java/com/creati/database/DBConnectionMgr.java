cf06dc0633818dd3dc7a99ca63f43be5b58c9163
jun248248 <20203110@office.deu.ac.kr>
2026-02-26 11:48:56 +0900

    DB접속 정보 수정

--- src/main/java/com/creati/database/DBConnectionMgr.java
+++ src/main/java/com/creati/database/DBConnectionMgr.java
@@ -39,9 +39,9 @@ import java.util.Vector;
 public class DBConnectionMgr {
     private Vector connections = new Vector(10);
     private String _driver = "com.mysql.cj.jdbc.Driver",
-    _url = "jdbc:mysql://localhost:3306/creati?characterEncoding=UTF-8&serverTimezone=UTC",
+    _url = "jdbc:mysql://cdn.jspstudy.co.kr:8306/creati?characterEncoding=UTF-8&serverTimezone=UTC",
     _user = "root",
-    _password = "1234";
+    _password = "dita2414";
     
     private boolean _traceOn = false;
     private boolean initialized = false;
